e53291ec4c5868c7005a4a4b6dc19822cc864d1b
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-27 11:44:21 +0100

    create show card

--- resources/views/admin/words/show.blade.php
+++ resources/views/admin/words/show.blade.php
@@ -1,1 +1,10 @@
 @extends('layouts.app')
+
+<div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{$word->term}}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">{{$word->slug}}</h6>
+      <p class="card-text">{{$word->definition}}</p>
+      <a href="{{route('admin.words.index')}}" class="card-link">Torna alla lista delle parole</a>
+    </div>
+  </div>
