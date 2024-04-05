349867856439648fb024137c1807455c57093820
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-04-05 15:21:44 +0200

    crud

--- app/Http/Controllers/Admin/LinkController.php
+++ app/Http/Controllers/Admin/LinkController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\Link;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class LinkController extends Controller
 {
